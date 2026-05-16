ae6afeebe3ac6527788dbd571e5956057ac48a5a
abenez3r1-create <abenez3r1@gmail.com>
2026-05-16 08:36:57 +0300

    initial backend

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "It's working",
+  });
+});
+app.post("/payment/create", async (req, res) => {
+  try {
+    const total = req.query.total;
+    if (total > 0) {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+      });
+      res.status(201).send({
+        clientSecret: paymentIntent.id,
+      });
+    } else {
+      res.status(403).send({
+        message: "Total must be greater than 0",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+    });
+  }
+});
+app.listen(3000, (req, res) => {
+  console.log("Server running on port 3000");
+});
